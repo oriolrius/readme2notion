a3b130f25e657880f34b40bee592b03972af9068
Oriol Rius <oriol.rius@nexiona.com>
2024-07-20 17:47:49 +0200

    completely restructured
    Co-authored-by: Néstor <nestor@nstlopez.com>

--- notionUtils.js
+++ notionUtils.js
@@ -1,16 +1,30 @@
-import { readFile } from 'fs/promises'; // use fs/promises for readFile to use async/await syntax
+// @ts-check
+import fs from 'node:fs/promises'; // use fs/promises for readFile to use async/await syntax
 import { markdownToBlocks } from '@tryfabric/martian';
 import { Client } from '@notionhq/client';
 
+/**
+ * Reads a file and converts its content from markdown to Notion blocks.
+ *
+ * @param {string} inputFile - The path of the input file to read.
+ * @returns {Promise<Array>} - A promise that resolves to an array of Notion blocks.
+ * @throws {Error} - If there is an error reading the file.
+ */
 export const getReadmeBlocks = async (inputFile) => {
   try {
-    const data = await readFile(inputFile, 'utf8');
+    const data = await fs.readFile(inputFile, 'utf8');
     return markdownToBlocks(data);
   } catch (err) {
     throw new Error(`Error reading file: ${err.message}`);
   }
 };
 
+/**
+ * Deletes the content of a Notion page.
+ * @param {Object} notion - The Notion client object.
+ * @param {string} pageId - The ID of the page to delete the content from.
+ * @returns {Promise<void>} - A promise that resolves when the content is deleted successfully.
+ */
 export const deletePageContent = async (notion, pageId) => {
   try {
     console.log(`Fetching blocks for page ID: ${pageId}`);
@@ -46,11 +60,19 @@ export const deletePageContent = async (notion, pageId) => {
   }
 };
 
+/**
+ * Adds children blocks to a Notion page.
+ *
+ * @param {Object} notion - The Notion client object.
+ * @param {string} pageId - The ID of the page to add blocks to.
+ * @param {Array} blocks - The array of blocks to add.
+ * @returns {Promise<void>} - A promise that resolves when the blocks are successfully added.
+ */
 export const addChildrenBlocks = async (notion, pageId, blocks) => {
   try {
     console.log(`Adding blocks to page ID: ${pageId}`);
 
-    const response = await notion.blocks.children.append({
+    await notion.blocks.children.append({
       block_id: pageId,
       children: blocks,
     });
@@ -66,6 +88,15 @@ export const createNotionClient = (authToken) => {
   return new Client({ auth: authToken });
 };
 
+/**
+ * Creates a new page in a Notion database.
+ * @param {Object} notion - The Notion client object.
+ * @param {string} databaseId - The ID of the database where the page will be created.
+ * @param {string} title - The title of the page.
+ * @param {string} hostname - The hostname associated with the page.
+ * @param {string} commit_hash - The Git commit hash associated with the page.
+ * @returns - The ID of the newly created page.
+ */
 export const createPage = async (notion, databaseId, title, hostname, commit_hash) => {
   try {
     console.log(`Creating a new page in database ID: ${databaseId}`);
@@ -107,29 +138,77 @@ export const createPage = async (notion, databaseId, title, hostname, commit_has
     });
 
     console.log(`Successfully created (${response.id}) a new page in the database with ID: ${databaseId}`);
-    // console.log(response);
     return response.id;
   } catch (error) {
     console.error(`Error creating a new page in the database: ${error.message}`);
   }
 };
 
-export const updatePageProperties = async (notion, pageId, properties) => {
+/**
+ * Updates the properties of a page in Notion.
+ *
+ * @param {Object} notion - The Notion client object.
+ * @param {string} pageId - The ID of the page to update.
+ * @param {Object} options - The options for updating the page properties.
+ * @param {string} options.page_name - The new name of the page.
+ * @param {string} options.hostname - The hostname to update.
+ * @param {string} options.git_commit - The git commit to update.
+ * @returns {Promise<void>} - A promise that resolves when the properties are successfully updated.
+ */
+export const updatePageProperties = async (notion, pageId, { page_name, hostname, git_commit }) => {
   try {
     console.log(`Updating properties of the page with ID: ${pageId}`);
 
-    const response = await notion.pages.update({
+    const properties = {
+      Name: {
+        title: [
+          {
+            text: {
+              content: page_name
+            }
+          }
+        ]
+      },
+      Hostname: {
+        rich_text: [
+          {
+            type: "text",
+            text: {
+              content: hostname
+            }
+          }
+        ]
+      },
+      GitCommit: {
+        rich_text: [
+          {
+            type: "text",
+            text: {
+              content: git_commit
+            }
+          }
+        ]
+      }
+    }
+
+    await notion.pages.update({
       page_id: pageId,
       properties,
     });
 
     console.log(`Successfully updated properties of the page with ID: ${pageId}`);
-    // console.log(response);
   } catch (error) {
     console.error(`Error updating properties of the page: ${error.message}`);
   }
 }
 
+/**
+ * Finds the page ID in a Notion database based on the provided title.
+ * @param {Object} notion - The Notion client object.
+ * @param {string} databaseId - The ID of the Notion database.
+ * @param {string} title - The title of the page to find.
+ * @returns {Promise<string>} - The ID of the found page, or false if not found.
+ */
 export const findPageId = async (notion, databaseId, title) => {
   console.log(`Finding page ID in database ID: ${databaseId}`);
 
@@ -145,10 +224,9 @@ export const findPageId = async (notion, databaseId, title) => {
 
   if (response.results.length === 0) {
     console.log(`Page with name ${title} not found in database with ID ${databaseId}`);
-    return false;
+    return "Not found";
   }
 
   console.log(`Successfully found page ID in the database with ID: ${databaseId}`);
-  // console.log(response);
   return response.results[0].id;
 };
--- readme2notion.js
+++ readme2notion.js
@@ -1,18 +1,81 @@
-#!/usr/bin/env node
 // @ts-check
 
-import { readFile, symlinkSync, writeFile } from 'node:fs';
-import { markdownToBlocks } from '@tryfabric/martian';
-import { Client } from '@notionhq/client';
+import fs from 'node:fs';
 import { program } from 'commander';
-// import rc from 'rc';
-import { create } from 'node:domain';
 import { createPage, findPageId, getReadmeBlocks, deletePageContent, addChildrenBlocks, createNotionClient, updatePageProperties } from './notionUtils.js';
-import os from 'os'; 
-import { exit } from 'node:process';
+import os from 'os';
 
 const hostname = os.hostname();
 console.log(`Hostname: ${hostname}`);
+
+/**
+ * Converts a readme file to Notion blocks and updates a Notion page with the converted content.
+ * @param {string} inputFile - The path to the input readme file.
+ * @param {object} options - The options for the conversion process.
+ * @param {string} options.config - The path to the configuration file.
+ * @param {string} options.commit - The git commit hash associated with the conversion.
+ * @returns {Promise<void>} - A promise that resolves when the conversion is complete.
+ */
+async function readme2Notion(inputFile, options) {
+  console.log(`Input file: ${inputFile}`);
+  console.log(`Configuration file: ${options.config}`);
+
+  const { default: config } = await import(options.config, { with: { type: 'json' } });
+  console.log(config);
+  const page_name = `${hostname}/${config.name}`;
+  const notion = createNotionClient(config['notion-token']);
+
+  try {
+    // Read the input file and convert it to Notion blocks
+    const blocks = await getReadmeBlocks(inputFile);
+    console.log(`Read ${blocks.length} blocks from the input file`);
+
+    // If a page ID is provided, delete its content
+    if (config.pageId) {
+      await deletePageContent(notion, config.page_id);
+      await addChildrenBlocks(notion, config.page_id, blocks);
+    }
+
+    let page_id = await findPageId(notion, config.db_id, page_name);
+
+
+    if (page_id === "Not Found") {
+      page_id = await createPage(notion, config.db_id, page_name, hostname, options.commit);
+    } else {
+      await updatePageProperties(notion, page_id, {
+        page_name, hostname, git_commit: options.commit
+      });
+      await deletePageContent(notion, page_id);
+    }
+
+    if (blocks.length > 20) {
+      let chunks = [];
+      for (let i = 0; i < chunks.length; i += 20) {
+        chunks.push(blocks.slice(i, i + 20));
+      }
+
+      for (let chunk of chunks) {
+        await addChildrenBlocks(notion, page_id, chunk);
+      };
+    } else {
+      await addChildrenBlocks(notion, page_id, blocks);
+    }
+
+    config.page_id = page_id;
+
+
+    fs.writeFile(options.config, JSON.stringify(config, null, 2), (err) => {
+      if (err) {
+        console.error(`Error saving configuration: ${err}`);
+      } else {
+        console.log(`Configuration saved with page ID: ${page_id}`);
+      }
+    });
+
+  } catch (error) {
+    console.error(`Error processing input file: ${error}`);
+  }
+}
 
 // Define the command-line options and arguments
 program
@@ -22,104 +85,7 @@ program
   .option('-c, --config <file_path_to_config_file>', 'JSON Config file path', '/repos/.notion.json')
   .option('-t, --commit <commit_hash>', 'The latest GIT commit hash', 'null')
   .option('-n, --notion-token <token>', 'Notion API token')
-
-  .action(async (inputFile, options) => { // make this function async
-    // Your main script logic here
-    console.log(`Input file: ${inputFile}`);
-    console.log(`Configuration file: ${options.config}`);
-
-    // Load the configuration file
-    let { default: config } = await import(options.config, { with: { type: 'json' }});
-    // import config from options.config assert { type: 'json' };
-    // try {
-    //   config = await import(options.config, { assert: { type: 'json' }}) && {
-    //     name: "",
-    //     db_id: "",
-    //     page_id: "",
-    //   };
-    // } catch (error) {
-    //   if ( error.code === 'ERR_MODULE_NOT_FOUND' ) {
-    //     console.error(`Configuration file not found: ${options.config}`);
-    //     exit(1);
-    //   } 
-    //   throw error;
-    // }
-
-    console.log(config);
-
-    const page_name = `${hostname}/${config.name}`;
-    const notion = createNotionClient(config['notion-token']);
-
-    try {
-      // Read the input file and convert it to Notion blocks
-      const blocks = await getReadmeBlocks(inputFile);
-      console.log(`Read ${blocks.length} blocks from the input file`);
-
-      // If a page ID is provided, delete its content
-      if (config.pageId) {
-        await deletePageContent(notion, config.page_id);
-        await addChildrenBlocks(notion, config.page_id, blocks);
-      } else {
-        // find the page ID based on the database ID and page name
-        let page_id = await findPageId(notion, config.db_id, page_name);
-        if (!page_id) {
-          page_id = await createPage(notion, config.db_id, page_name, hostname, options.commit);
-        } else {
-          await updatePageProperties(notion, page_id, {
-            Name: {
-              title: [
-                {
-                  text: {
-                    content: page_name
-                  }
-                }
-              ]
-            },
-            Hostname: {
-              rich_text: [
-                {
-                  type: "text",
-                  text: {
-                    content: hostname
-                  }
-                }
-              ]
-            },
-            GitCommit: {
-              rich_text: [
-                {
-                  type: "text",
-                  text: {
-                    content: options.commit
-                  }
-                }
-              ]
-            }
-          });
-          await deletePageContent(notion, page_id);
-        }
-        await addChildrenBlocks(notion, page_id, blocks);
-        // save page_id in the rc file
-        config.page_id = page_id;
-        // remove unneded config fields
-        delete config.config;
-        delete config.configs;
-        delete config._;
-        delete config.commit;
-        writeFile(options.config, JSON.stringify(config, null, 2), (err) => {
-          if (err) {
-            console.error(`Error saving configuration: ${err}`);
-          } else {
-            console.log(`Configuration saved with page ID: ${page_id}`);
-          }
-        });
-      }
-
-
-    } catch (error) {
-      console.error(`Error processing input file: ${error}`);
-    }
-  });
+  .action(readme2Notion);
 
 // Parse the command-line arguments
 program.parse(process.argv);
